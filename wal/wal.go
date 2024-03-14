22890aef3327dd8259849ad3e4231cb3704e4d03
suohailong <suohailong@qianxin.com>
2024-03-14 19:30:55 +0800

    refactor: refactor the code

--- wal/wal.go
+++ wal/wal.go
@@ -1,13 +1,11 @@
 package wal
 
 import (
-	"bytes"
 	"encoding/binary"
 	"encoding/json"
 	"hash/crc32"
 	"os"
 	"sync"
-	"time"
 
 	"distributed-learning-lab/util/log"
 )
@@ -27,136 +25,83 @@ func (d *defaultSerializer) Unmarshal(data []byte, v any) error {
 	return json.Unmarshal(data, v)
 }
 
-type EntryType uint8
-
-const (
-	EntryTypePut    EntryType = iota
-	EntryTypeUpdate EntryType = iota
-	EntryTypeDelete EntryType = iota
-)
-
-type WalEntry struct {
-	EntryIndex uint64    `json:"entryIndex"`
-	Key        []byte    `json:"key"`
-	Value      []byte    `json:"value"`
-	EntryType  EntryType `json:"entryType"`
-	TimeStamp  uint64    `json:"timeStamp"`
-}
-
-// TODO: 什么时候刷新到磁盘, etcd相当于小批量刷新
+// TODO: 什么时候刷新到磁盘, 立即刷新, 因为是wal，不立即刷新到磁盘就没有持久化可言
 // TODO: 如果日志文件损坏了，怎么办
 // TODO: 日志文件是append only， 如果客户端由于网络等原因失败后重试， 需要append 提供幂等性
-// TODO: writeAheadLog 日志低于低水位线的可以删掉
-type WriteAheadLog struct {
+// TODO: Wal 日志低于低水位线的可以删掉
+type Wal struct {
 	sync.RWMutex
-	index    int
-	entries  []*WalEntry
-	file     *os.File
-	ds       Serializer
+	index int
+	// entities []byte
+	file *os.File
+	// ds       Serializer
 	crcTable *crc32.Table
 }
 
-func NewWriteAheadLog(logName string) *WriteAheadLog {
+func CreateWal(logName string) *Wal {
 	f, err := os.OpenFile(logName, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0o644)
 	if err != nil {
 		log.Fatalf("open file %s failed: %v", logName, err)
 	}
 	log.Debugf("open file: %s successfully", logName)
 
-	wal := &WriteAheadLog{
+	wal := &Wal{
 		index:    0,
-		entries:  make([]*WalEntry, 0),
 		file:     f,
-		ds:       &defaultSerializer{},
 		crcTable: crc32.MakeTable(crc32.Castagnoli),
 	}
-
-	// 定时刷新到磁盘
-	go func() {
-		for {
-			time.Sleep(1 * time.Second)
-			wal.SaveToDisk()
-		}
-	}()
 
 	return wal
 }
 
-func (wal *WriteAheadLog) ReadLogAll() ([]*WalEntry, error) {
-	wal.Lock()
-	defer wal.Unlock()
+func (w *Wal) ReadAll() ([]byte, error) {
+	w.Lock()
+	defer w.Unlock()
 
-	stat, err := wal.file.Stat()
+	stat, err := w.file.Stat()
 	if err != nil {
 		return nil, err
 	}
 
 	buf := make([]byte, stat.Size())
-	_, err = wal.file.Read(buf)
+	_, err = w.file.Read(buf)
 	if err != nil {
 		return nil, err
 	}
 
-	entries := make([]*WalEntry, 0)
-	for _, line := range bytes.Split(buf, []byte("\n")) {
-		if len(line) == 0 {
-			continue
-		}
-		entry := &WalEntry{}
-		err = wal.ds.Unmarshal(line, entry)
-		if err != nil {
-			return nil, err
-		}
-		entries = append(entries, entry)
-	}
+	// entries := make([]byte, 0)
+	// for _, line := range bytes.Split(buf, []byte("\n")) {
+	// 	if len(line) == 0 {
+	// 		continue
+	// 	}
+	// 	entries = append(entries, entry...)
+	// }
 
-	// wal.entries = entries
-	wal.index = len(entries)
-	return entries, nil
+	// w.entries = entries
+	// w.index = len(entries)
+	return buf, nil
 }
 
-func (wal *WriteAheadLog) WriteEntry(key, value string, entryType EntryType) {
-	wal.Lock()
-	defer wal.Unlock()
-	entry := &WalEntry{
-		EntryIndex: uint64(wal.index),
-		TimeStamp:  uint64(time.Now().Unix()),
-		Key:        []byte(key),
-		Value:      []byte(value),
-		EntryType:  entryType,
-	}
-	wal.entries = append(wal.entries, entry)
-	wal.index++
+func (w *Wal) sync() error {
+	return w.file.Sync()
 }
 
-func (wal *WriteAheadLog) SaveToDisk() error {
-	wal.RLock()
-	defer wal.RUnlock()
-
-	if len(wal.entries) == 0 {
+func (w *Wal) save(entities [][]byte) error {
+	w.Lock()
+	defer w.Lock()
+	if len(entities) == 0 {
 		return nil
 	}
-
-	for _, entry := range wal.entries {
-		byteData, err := wal.ds.Marshal(entry)
+	for _, entity := range entities {
+		entryCrc := crc32.Checksum(entity, w.crcTable)
+		_, err := w.file.Write(entity)
 		if err != nil {
-			log.Fatalf("Failed to marshal entry: %v", err)
+			return err
 		}
-		entryCrc := crc32.Checksum(byteData, wal.crcTable)
-		_, err = wal.file.Write(byteData)
+		err = binary.Write(w.file, binary.LittleEndian, entryCrc)
 		if err != nil {
-			log.Fatalf("Failed to write entry to file: %v", err)
+			return err
 		}
-
-		binary.Write(wal.file, binary.LittleEndian, entryCrc)
 	}
-	// 这里同步完就删掉当前的日志
-	wal.entries = make([]*WalEntry, 0)
-
-	err := wal.file.Sync()
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return w.sync()
 }
